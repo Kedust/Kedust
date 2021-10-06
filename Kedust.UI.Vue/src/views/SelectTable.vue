0a9fbc491456b330fcabace21e1bb92d56a38e38
Bernd De Baere <bernd.debaere@actemium.be>
2021-10-06 16:31:37 +0200

    cleanup SelectTable

--- Kedust.UI.Vue/src/views/SelectTable.vue
+++ Kedust.UI.Vue/src/views/SelectTable.vue
@@ -2,17 +2,19 @@
   <div class="page">
     <div class="back-button" v-if="tableAvailable" @click="goToMenu"><i class="material-icons">arrow_back</i></div>
 
-    <form @submit="setTableCode">
+    <form @submit="submit">
       <div class="input-field">
         <input id="table_code" type="text" v-model="input">
         <label for="table_code" class="active">Tafelcode</label>
       </div>
 
-      <div class="button primary-bg" @click="setTableCode">Naar de kaart!</div>
+      <div class="button primary-bg" @click="submit">Naar de kaart!</div>
 
     </form>
 
-    <qrcode-stream @decode="onDecode"></qrcode-stream>
+    <div class="button primary-bg" v-if="!scanCode" @click="scanCode = true">Scan QR code</div>
+
+    <qrcode-stream @decode="onDecode" v-if="scanCode"></qrcode-stream>
 
 
   </div>
@@ -26,10 +28,10 @@
 </style>
 
 <script>
-import {mapMutations, mapActions, mapGetters} from "vuex";
+import {mapMutations, mapGetters} from "vuex";
 import Materialize from "materialize-css";
 import Gateway from "@/gateway";
-import { QrcodeStream } from 'vue-qrcode-reader'
+import {QrcodeStream} from 'vue-qrcode-reader'
 
 export default {
   name: 'SelectTable',
@@ -38,10 +40,11 @@ export default {
   },
   data() {
     return {
+      scanCode: false,
       input: this.table
     }
   },
-  components:{
+  components: {
     QrcodeStream
   },
   computed: {
@@ -52,21 +55,8 @@ export default {
   },
   async mounted() {
     const table = this.$route.params.code;
-    console.log(table);
     if (table !== undefined) {
-      Gateway.Table.checkCode(table).then(async (result) => {
-        if (result) {
-          this.setTable(table);
-          await this.updateMenu();
-          await this.$router.push({name: 'Menu'});
-        } else {
-          Materialize.toast({
-            html: "Sorry, die tafel vinden we niet terug...</br>Geef de code hierboven in alstublieft",
-            classes: "toast-danger"
-          });
-          this.loading(false);
-        }
-      });
+      await this.setTableCode(table);
     }
   },
   methods: {
@@ -75,40 +65,19 @@ export default {
       setMenu: "setMenu",
       loading: "setLoading"
     }),
-    ...mapActions({
-      checkTable: "checkTable",
-      updateMenu: "updateMenu"
-    }),
-    onDecode(data){
-      if(data.toLowerCase().startsWith("https://kedust.be/table/")){
-        let scanedTable = data.substring("https://kedust.be/table/".length)
-        Gateway.Table.checkCode(scanedTable).then(async (result) => {
-          if (result) {
-            alert(scanedTable);
-            this.setTable(scanedTable);
-            await this.updateMenu();
-            await this.$router.push({name: 'Menu'});
-          } else {
-            Materialize.toast({
-              html: "Sorry, die tafel vinden we niet terug...</br>Geef de code hierboven in alstublieft",
-              classes: "toast-danger"
-            });
-            this.loading(false);
-          }
-        });
+    onDecode(data) {
+      if (data.toLowerCase().startsWith("https://kedust.be/table/")) {
+        let code = data.substring("https://kedust.be/table/".length)
+        this.setTableCode(code);
       }
     },
-    async goToMenu() {
-      await this.$router.push({name: "Menu"})
-    },
-    async setTableCode(e) {
-      e.preventDefault();
+    setTableCode(code) {
       this.loading(true);
-      Gateway.Table.checkCode(this.input)
+      Gateway.Table.checkCode(code)
           .then((response) => {
                 if (response) {
-                  this.setTable(this.input);
-                  return Gateway.Menu.getByTableCode(this.input).then(async (choicesResult) => {
+                  this.setTable(code);
+                  return Gateway.Menu.getByTableCode(code).then(async (choicesResult) => {
                     this.setMenu(choicesResult)
                     this.loading(false);
                     await this.$router.push({name: 'Menu'});
@@ -119,6 +88,13 @@ export default {
                 }
               }
           );
+    },
+    async submit(e) {
+      e.preventDefault();
+      this.setTableCode(this.input);
+    },
+    async goToMenu() {
+      await this.$router.push({name: "Menu"})
     }
   }
 }
