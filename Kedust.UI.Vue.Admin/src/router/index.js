33bddba158f8d86175e780d606f5b3c0292b816a
BerndDeBaere <bernddebaere@hotmail.com>
2021-11-16 22:44:14 +0100

    Simpele login

--- Kedust.UI.Vue.Admin/src/router/index.js
+++ Kedust.UI.Vue.Admin/src/router/index.js
@@ -6,52 +6,63 @@ import Settings from "@/views/Settings";
 import Menu from "@/views/Menu";
 import Dashboard from "@/views/Dashboard";
 import Table from "@/views/Table";
+import Login from "@/views/Login";
 
 const routes = [
-
-
-  {
-    path: '/',
-    name: 'Dashboard',
-    component: Dashboard
-  },{
-    path: '/Orders',
-    name: 'Orders',
-    component: Orders
-  },{
-    path: '/Settings',
-    name: 'Settings',
-    component: Settings
-  },{
-    path: '/Tables',
-    name: 'Tables',
-    component: Tables
-  },{
-    path: '/Table/:id',
-    name: 'TableEdit',
-    component: Table
-  },{
-    path: '/Table',
-    name: 'TableNew',
-    component: Table
-  },{
-    path: '/Menus',
-    name: 'Menus',
-    component: Menus
-  },{
-    path: '/Menu',
-    name: 'MenuNew',
-    component: Menu
-  },{
-    path: '/Menu/:id',
-    name: 'MenuEdit',
-    component: Menu
-  }
+    {
+        path: '/',
+        name: 'Dashboard',
+        component: Dashboard
+    }, {
+        path: '/Orders',
+        name: 'Orders',
+        component: Orders
+    }, {
+        path: '/Settings',
+        name: 'Settings',
+        component: Settings
+    }, {
+        path: '/Tables',
+        name: 'Tables',
+        component: Tables
+    }, {
+        path: '/Table/:id',
+        name: 'TableEdit',
+        component: Table
+    }, {
+        path: '/Table',
+        name: 'TableNew',
+        component: Table
+    }, {
+        path: '/Menus',
+        name: 'Menus',
+        component: Menus
+    }, {
+        path: '/Menu',
+        name: 'MenuNew',
+        component: Menu
+    }, {
+        path: '/Menu/:id',
+        name: 'MenuEdit',
+        component: Menu
+    }, {
+        path: '/Login',
+        name: 'Login',
+        component: Login
+    }
 ]
 
 const router = createRouter({
-  history: createWebHashHistory(),
-  routes
+    history: createWebHashHistory(),
+    routes
+})
+router.beforeEach((to, _, next) => {
+    console.log(localStorage.getItem("userToken"))
+
+    if (to.name !== "Login" && localStorage.getItem("loggedIn") === null) {
+        console.log("NOT LOGED IN")
+        next({name: "Login"})
+    } else next();
 })
 
 export default router
---
+++ Kedust.UI.Vue.Admin/src/views/Login.vue
@@ -0,0 +1,31 @@
+<template>
+  <h1>Login</h1>
+  <div class="input-field">
+    <input id="password" type="password" v-model="password">
+    <label for="password">Password</label>
+  </div>
+  <button class="btn" @click="login()">Login</button>
+</template>
+
+<script>
+export default {
+  name: "Login",
+  data(){
+    return{
+      password:""
+    }
+  },
+  methods:{
+    login(){
+      if(this.password === "Lekker")
+        localStorage.setItem("loggedIn", "")
+      else
+        localStorage.removeItem("loggedIn")
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
